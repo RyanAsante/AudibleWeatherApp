00e6ce6483dd03c01a9bbc8804a78134a3db3c9a
RyanAsante <141275850+RyanAsante@users.noreply.github.com>
2024-07-31 10:58:36 -0400

    Added weatherDataResponse to retrieve data from weatherApi

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -1,20 +1,21 @@
-package $org.example;
+package org.example;
 
-import.java.util.Scanner;
-import org.apache.http.HttpEntity;
-import org.apache.http.entity.BasicHttpEntity;
+import java.util.Scanner;
+//import org.apache.http.HttpEntity;
+//import org.apache.http.entity.BasicHttpEntity;
 
-public class WeatherAppData {
+public class App {
 
-    priavte String date;
+    private String date;
     private String locationName;
-    priavte String metric;
+    private String measurement;
     private final String API_KEY = "YMX5CP95D2LLVM4P4ABH5WNN4";
+    private String apiEndPoint = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
 
-    public WeatherAppData (String Weatherdate, String WeatherlocationName, String Weathermetric) {
+    public App (String Weatherdate, String WeatherlocationName, String Weathermeasurement) {
         date = Weatherdate;
         locationName = WeatherlocationName;
-        metric = Weathermetric;
+        measurement = Weathermeasurement;
     }
 
    public String getDate() {
@@ -23,8 +24,11 @@ public class WeatherAppData {
    public String getLocationName() {
         return locationName;
    }
-   public String getMetric() {
-        return metric;
+   public String getMeasurement() {
+        return measurement;
+   }
+   public String getApiEndPoint() {
+      return apiEndPoint;
    }
 
 }
--- src/main/resources/archetype-resources/src/main/java/WeatherAppUserData.java
+++ src/main/resources/archetype-resources/src/main/java/WeatherAppUserData.java
@@ -1,36 +1,71 @@
-package $org.example;
+package org.example;
 
-import.java.util.Scanner;
+import java.util.Scanner;
 import org.apache.http.HttpEntity;
 import org.apache.http.entity.BasicHttpEntity;
+import java.lang.StringBuilder;
+import java.net.URL;
 
 public class WeatherAppUserData {
-    Scanner scanner = new Scanner(System.in);
 
-    String userLocation;
-    String userMetric;
-    Strind userDate;
+    public static void main(String[] args) {
 
-    System.out.println("Please enter your location: ");
-    userLocation = scanner.nextLine();
+        Scanner scanner = new Scanner(System.in);
 
-    System.out.println("Please enter the metric you prefer: ");
-    userMetric = scanner.nextLine();
+        String userLocation;
+        String userMeasurement;
+        String userDate;
+        String API_KEY = "YMX5CP95D2LLVM4P4ABH5WNN4";
+        String apiEndPoint = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
 
-     System.out.println("Please enter the date: ");
-     userDate = scanner.nextLine();
+        String method = "GET";
 
-    WeatherAppData data = new WeatherAppData (userDate,userLocation,userMetric);
+        System.out.println("Please enter your location: ");
+        userLocation = scanner.nextLine();
+
+        System.out.println("Please enter the measurement you prefer (us, uk, metric, or base): ");
+        userMeasurement = scanner.nextLine();
+
+        System.out.println("Please enter the date: ");
+        userDate = scanner.nextLine();
+
+            String urlLink = apiEndPoint + userLocation;
+
+            StringBuilder keyParameters = new StringBuilder();
+            keyParameters.append("&").append("unitGroup=").append(userMeasurement);
+            keyParameters.append("&").append("key=").append(API_KEY);
+
+            if ("GET".equals(method)) {
+                urlLink.append("?").append(keyParameters);
+
+                System.out.println(urlLink);
+
+            }
 
 
+try {
+    URL url = new URL(urlLink);
+    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+    connection.setRequestMethod("GET");
 
 
+    int responseNum = connection.getResponseCode();
+
+    if (responseNum == 200) {
+        StringBuilder weatherDataResponse = new StringBuilder();
+        Scanner scanner2 = new Scanner(connection.getInputStream());
+        while (scanner2.hasNext()) {
+            weatherDataResponse.append(scanner2.nextLine());
+        }
 
 
     }
+}
+        catch(Exception e){
+            System.out.println("Error: Could not connect to API. Please try again.");
 
-
-
+        }
+    }
 
 }
 
